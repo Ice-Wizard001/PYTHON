57d61c6a1e9571d19584b750c509571936313a0c
Code With Asad <rasadullah75@gmail.com>
2025-08-21 00:58:45 -0800

    Update 2nd practical.py

--- 100 DAYS SERIES  25 march/Chalenge 2/2nd practical.py
+++ 100 DAYS SERIES  25 march/Chalenge 2/2nd practical.py
@@ -2,8 +2,16 @@ import time
 timestamp=time.strftime('%H:%M:%S')
 print(timestamp)
 
-timestamp1=time.strftime('%H')
+timestamp1=int(time.strftime('%H'))
 
-if timestamp1 <=7:
+if timestamp1 <12:
     print("Good Morning Sir")
 
+elif 12<= timestamp1 <17:
+    print("Good Afternoon")
+
+elif 17<= timestamp1 <20:
+    print("Good Evening")
+
+else:
+    print("Good Night")
